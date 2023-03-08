1d104b25d031ebf5a510bede8112e86631f48bc2
Tiago Bernardi <107669286+Tiago-Bernardi@users.noreply.github.com>
2023-03-08 14:43:28 -0300

    Add files via upload

---
+++ Java01Aula04PrimeiroPrograma/src/java01aula04primeiroprograma/Java01Aula04PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package java01aula04primeiroprograma;
+
+public class Java01Aula04PrimeiroPrograma {
+   
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+}
+
+// psvm = public static void main(String[] args)
+// sout = System.out.println
